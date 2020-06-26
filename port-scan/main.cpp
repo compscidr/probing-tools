dfbff1dbef6489f626b197a6f4f77df2016b9c37
Jason Ernst <jason@left.io>
2020-06-26 10:33:02 -0700

    convert in_addr to sockaddr_in

--- port-scan/main.cpp
+++ port-scan/main.cpp
@@ -4,7 +4,7 @@
 int main() {
 
     udp_scanner udp;
-    udp.scan("10.0.0.116", "10.0.0.118", 1001, 1005, "test", 4);
+    udp.scan("10.0.0.116", "10.0.0.116", 5000, 5001, "test", 4);
 
     return 0;
 }
--- port-scan/udp_scanner.cpp
+++ port-scan/udp_scanner.cpp
@@ -13,32 +13,36 @@ udp_scanner::udp_scanner() {
 void udp_scanner::scan(std::string start_address, std::string end_address, int start_port, int end_port, const char *payload, const int payload_len) {
     std::cout << "Starting scan from " << start_address << "-" << end_address << ":" << start_port << "-" << end_port << std::endl;
 
-    in_addr start_in_addr;
-    in_addr end_in_addr;
+    sockaddr_in start_sockaddr_in;
+    sockaddr_in end_sockaddr_in;
 
-    if(!inet_aton(start_address.c_str(), &start_in_addr)) {
+    if(!inet_aton(start_address.c_str(), &start_sockaddr_in.sin_addr)) {
         throw std::runtime_error("Invalid starting address: " + start_address);
     }
-    if(!inet_aton(end_address.c_str(), &end_in_addr)) {
+    if(!inet_aton(end_address.c_str(), &end_sockaddr_in.sin_addr)) {
         throw std::runtime_error("Invalid ending address: " + end_address);
     }
 
     int send_socket = socket(AF_UNSPEC, SOCK_DGRAM | SOCK_CLOEXEC, IPPROTO_UDP);
 
-    while (start_in_addr.s_addr <= end_in_addr.s_addr) {
-        std::cout << "Scanning: " << inet_ntoa(start_in_addr) << std::endl;
+    while (start_sockaddr_in.sin_addr.s_addr <= end_sockaddr_in.sin_addr.s_addr) {
+        std::cout << "Scanning: " << inet_ntoa(start_sockaddr_in.sin_addr) << std::endl;
 
         int current_port = start_port;
         while (current_port <= end_port) {
             std::cout << "  Scanning port: " << current_port << std::endl;
-
-            sendto(send_socket, payload, payload_len, 0, (struct sockaddr*)&start_in_addr, sizeof(start_in_addr));
+            start_sockaddr_in.sin_family = AF_INET;
+            start_sockaddr_in.sin_port = current_port;
+            if(!sendto(send_socket, payload, payload_len, 0, (struct sockaddr*)&start_sockaddr_in, sizeof(start_sockaddr_in))) {
+                throw std::runtime_error("Failed sending UDP packet to "
+                    + std::string(inet_ntoa(start_sockaddr_in.sin_addr)) + ":" + std::to_string(current_port));
+            }
 
             current_port++;
         }
 
-        int temp = ntohl(start_in_addr.s_addr) + 1;
-        start_in_addr.s_addr = htonl(temp);
+        int temp = ntohl(start_sockaddr_in.sin_addr.s_addr) + 1;
+        start_sockaddr_in.sin_addr.s_addr = htonl(temp);
     }
 }
 
