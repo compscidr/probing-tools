3385a91b5a57001702bfd79dbe7d0f601ec5e9ff
Jason Ernst <ernstjason1@gmail.com>
2020-07-09 20:15:02 -0700

    Added select to scan multiple ports / hosts at once

--- port-scan/tcp_scanner.cpp
+++ port-scan/tcp_scanner.cpp
@@ -10,6 +10,10 @@
 #include <iostream>
 #include <zconf.h>
 #include <arpa/inet.h>
+#include <fcntl.h>
+#include <sys/poll.h>
+#include <err.h>
+#include <cstring>
 #include "tcp_scanner.h"
 
 // NOTE: only works for IPv4.  Check out inet_pton/inet_ntop for IPv6 support.
@@ -32,17 +36,18 @@ char* increment_address(const char* address_string)
     return inet_ntoa(address_struct);
 }
 
+//influenced by: http://mff.devnull.cz/pvu/src/tcp/non-blocking-connect.c
 void tcp_scanner::scan(std::string start_address, std::string end_address, int start_port, int end_port) {
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        throw std::runtime_error("Error opening socket");
-    }
-
     int success = 0;
     in_addr_t sa = inet_addr(start_address.c_str());
     in_addr_t ea = inet_addr(end_address.c_str());
+    fd_set wrfds, wrfds_orig;
+    struct timeval tout;
+    FD_ZERO(&wrfds);
     sa = ntohl(sa);
     ea = ntohl(ea);
+    int count = 0;
+    int n = 0;
     while(sa <= ea) {
         struct hostent * server;
         server = gethostbyname(start_address.c_str());
@@ -58,14 +63,30 @@ void tcp_scanner::scan(std::string start_address, std::string end_address, int s
         int s = start_port;
 
         while(s <= end_port) {
+            int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+            if (sockfd < 0) {
+                throw std::runtime_error("Error opening socket");
+            }
+            fcntl(sockfd, F_SETFL, O_NONBLOCK);
+            count++;
             serv_addr.sin_port = htons(s);
 
-            if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-                std::cout << "Failed to connect to " << start_address << ":" << s << std::endl;
+            std::pair<int, tcp_scan> pair(sockfd, tcp_scan(s, start_address, sockfd));
+            scan_map.insert(pair);
+            if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1) {
+                //we expect this to happen
+                if(errno == EINPROGRESS) {
+                    //std::cout << "Connect in progress" << std::endl;
+                    FD_SET(sockfd, &wrfds);
+                } else {
+                    //may happen on localhost
+                    std::cout << "Connect error: " << strerror(errno) << std::endl;
+                    ++n;
+                }
             } else {
-                std::cout << "Connected to " << start_address << ":" << s << std::endl;
-                close(sockfd);
-                success++;
+                //may happen on localhost
+                std::cout << "Connect error: " << strerror(errno) << std::endl;
+                ++n;
             }
             s++;
         }
@@ -73,5 +94,48 @@ void tcp_scanner::scan(std::string start_address, std::string end_address, int s
         sa = inet_addr(start_address.c_str());
         sa = ntohl(sa);
     }
+
+    wrfds_orig = wrfds;
+    tout.tv_sec = 10;
+    tout.tv_usec = 0;
+    int timeouts = 0;
+
+    while(n != count) {
+        int error = select(FD_SETSIZE, NULL, &wrfds, NULL, &tout);
+        if (error == 0) {
+            timeouts++;
+            std::cout << "Timeout" << std::endl;
+            tout.tv_sec = 1;
+            tout.tv_usec = 0;
+            continue;
+        } else {
+            if (error == -1)
+                err(1, "select");
+        }
+
+        for (auto it : scan_map) {
+            if (!FD_ISSET(it.first, &wrfds)) {
+                continue;
+            }
+
+            int optval = -1;
+            socklen_t optlen = sizeof(optval);
+
+            if (getsockopt(it.second.get_sockfd(), SOL_SOCKET, SO_ERROR, &optval, &optlen) == -1)
+                err(1, "getsockopt");
+
+            if (optval == 0) {
+                success++;
+                std::cout << "Connected to " << it.second.get_host() << ":" << it.second.get_port() << std::endl;
+            } else {
+                //std::cout << "Failed connect to " << it.second.get_host() << ":" << it.second.get_port() << std::endl;
+            }
+            FD_CLR(it.first, &wrfds_orig);
+            close(it.second.get_sockfd());
+            ++n;
+        }
+        wrfds = wrfds_orig;
+    }
+
     std::cout << "Success: " << success << std::endl;
 }
--- port-scan/tcp_scanner.h
+++ port-scan/tcp_scanner.h
@@ -6,10 +6,24 @@
 #define PROBING_TOOLS_TCP_SCANNER_H
 
 #include <string>
+#include <unordered_map>
 
 class tcp_scanner {
+    class tcp_scan {
+        int port;
+        std::string host;
+        int sockfd;
+    public:
+        tcp_scan(int port, std::string host, int sockfd) {this->port = port; this->host = host; this->sockfd = sockfd; }
+        int get_port() const { return port; }
+        std::string get_host() const { return host; }
+        int get_sockfd() const { return sockfd; }
+    };
 public:
     void scan(std::string start_address, std::string end_address, int start_port, int end_port);
+private:
+    //mapping of sockfd -> tcp_scan
+    std::unordered_map<int, tcp_scan> scan_map;
 };
 
 
